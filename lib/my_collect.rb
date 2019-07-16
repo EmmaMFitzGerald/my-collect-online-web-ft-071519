f417e78273a28548836592b8c1cb6c3beda84aa5
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-16 16:15:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   new_array = []
   i = 0
   while i < array.length 
-    new_array << yield(array[i].capitalize) 
+    new_array << yield(array[i].capitalize)
     i += 1
   end
 end
